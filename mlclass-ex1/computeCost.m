fc0d99d7ab5ad1d6ca4b5a6fe876038b3987d8e0
Herbert <loveltyoic@gmail.com>
2014-08-28 17:36:40 +0800

    compute

--- mlclass-ex1/computeCost.m
+++ mlclass-ex1/computeCost.m
@@ -6,7 +6,7 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
@@ -14,7 +14,9 @@ J = 0;
 %               You should set J to the cost.
 
 
+diff = X * theta - y;
 
+J = sum(diff.^2)/(2*m);
 
 
 % =========================================================================
