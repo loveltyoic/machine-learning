9adc0374fe803f6bb9dc5f4b81bf263bc3f67d43
Herbert <loveltyoic@gmail.com>
2014-08-29 14:30:02 +0800

    complete ex1

--- mlclass-ex1/computeCostMulti.m
+++ mlclass-ex1/computeCostMulti.m
@@ -6,13 +6,16 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+diff = X * theta - y;
+
+J = sum(diff.^2)/(2*m);
 
 
 
