9a697bbfb26ab7de94f86ff6dadcc3ada353712f
Herbert <loveltyoic@gmail.com>
2014-09-02 17:10:37 +0800

    update

--- mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,17 +62,40 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Part 1
+
+X = [ones(m, 1), X]';
+
+z2 = Theta1 * X;
+
+a2 = sigmoid(z2);
+
+a2 = [ones(1, m); a2];
+
+z3 = Theta2 * a2;
+
+h = sigmoid(z3);
+
+temp = zeros(num_labels, m);
+
+for i = 1:m
+  temp(y(i), i) = 1
+end
+
+y = temp;
+
+J = 1/m * sum(sum(-y.*log(h)-(1-y).*log(1-h)));
+
+
+% Part 3
+
+Theta1 = Theta1(:, 2:end);
+Theta2 = Theta2(:, 2:end);
+J = J + lambda/(2*m) * (sum(sum(Theta1.^2))+sum(sum(Theta2.^2)));
 
 
 
-
-
-
-
-
-
-
-
+% Part 2
 
 
 
